521d4e91bbce1fbc94830091878aa42e81260589
djuricam <djuricam@Djuricam-PC.lanaco.com>
2020-10-24 22:21:12 +0200

    index page

--- src/main/java/com/github/nemojmenervirat/flightadvisor/controller/AppController.java
+++ src/main/java/com/github/nemojmenervirat/flightadvisor/controller/AppController.java
@@ -1,7 +1,10 @@
 package com.github.nemojmenervirat.flightadvisor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.nemojmenervirat.flightadvisor.repository.AirportRepository;
@@ -23,6 +26,21 @@ public class AppController {
 	private CommentRepository commentRepository;
 	@Autowired
 	private AppUserRepository appUserRepository;
+
+	@GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String index() {
+		//@formatter:off
+		return "<html>"
+				+ "<header>"
+					+ "<title>Flight Advisor</title>"
+				+ "</header>"
+				+ "<body>Detailed information about API can be found "
+					+ "<a href='https://github.com/nemojmenervirat/flight-advisor'>here.</a>" + 
+				" </body>"
+			+ "</html>";
+		//@formatter:on
+	}
 
 	@PostMapping("/reset")
 	public void reset() {
